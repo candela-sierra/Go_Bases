cc3c26227dd5df9544264b8729ccc669522d9d6f
candela-sierra <mariacandela.sierra@mercadolibre.com>
2024-02-28 15:06:39 -0300

    add exercise 1

---
+++ app/bin/exercises/1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nombre := "Candela"
+	direccion := "Calle Falsa 123"
+	fmt.Println("Nombre:", nombre)
+	fmt.Println("Direccion:", direccion)
+}
